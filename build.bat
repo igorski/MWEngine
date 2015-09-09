0c82fe4666a9290c6709e486463165c8677172cd
Igor Zinken <igor.zinken@igorski.nl>
2015-09-09 21:11:07 +0200

    updated build batch file

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ SET INPUT="-I.\jni"
 rm -rf src\nl\igorski\lib\audio\nativeaudio
 mkdir src\nl\igorski\lib\audio\nativeaudio
 
-swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src\nl\igorski\lib\audio\nativeaudio %INPUT% -o jni\java_interface_wrap_test.cpp jni\mwengine.i
+swig -java -package nl.igorski.lib.audio.nativeaudio -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src\nl\igorski\lib\audio\nativeaudio %INPUT% -o jni\jni\java_interface_wrap.cpp jni\mwengine.i
 
 ndk-build TARGET_PLATFORM=android-14 V=1
