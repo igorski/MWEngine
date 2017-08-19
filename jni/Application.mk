c1dacc028655de49f437c03ec1736c5952fdcb20
Igor Zinken <igor.zinken@igorski.nl>
2017-08-19 21:32:32 +0200

    removed rudimentary CPU architectures from makefile

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,7 +2,7 @@ APP_OPTIM      := release
 APP_STL        := gnustl_static
 APP_CPPFLAGS   += -std=c++11 -Werror -fexceptions -frtti
 #APP_CPPFLAGS   += -Wall
-APP_ABI        := armeabi armeabi-v7a x86 mips
+APP_ABI        := x86 x86_64 armeabi armeabi-v7a arm64-v8a
 
 ifeq ($(TARGET_ARCH_ABI), x86)
 LOCAL_CFLAGS += -m32
