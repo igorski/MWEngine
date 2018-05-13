2ffb1230f3e671c9ebafeb253b42081ba48a7e14
Igor Zinken <igor.zinken@gmail.com>
2018-05-13 19:27:26 +0200

    Added support to compile NDK project directly from Android Studio
    * Project now builds directly from Android Studio
    * CLI builds working
    * Removed rudimentary project properties file

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 %ECHO OFF
 
 REM the input folders for the C++ sources
-SET INPUT="-I.\jni"
+SET INPUT="-I.\src\main\cpp"
 
 rm -rf src\main\java\nl\igorski\lib\audio\mwengine
 mkdir src\main\java\nl\igorski\lib\audio\mwengine
 
-swig -java -package nl.igorski.lib.audio.mwengine -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src\main\java\nl\igorski\lib\audio\mwengine %INPUT% -o jni\jni\java_interface_wrap.cpp jni\mwengine.i
+swig -java -package nl.igorski.lib.audio.mwengine -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src\main\java\nl\igorski\lib\audio\mwengine %INPUT% -o src\main\cpp\jni\java_interface_wrap.cpp src\main\cpp\mwengine.i
 
-ndk-build V=1
+ndk-build -C .\src\main\cpp APP_BUILD_SCRIPT=Android.mk NDK_APPLICATION_MK=Application.mk NDK_LIBS_OUT=..\..\..\libs V=1
--- build.sh
+++ build.sh
@@ -4,11 +4,18 @@
 export ANDROID_NDK_ROOT=/Library/AndroidNDK
 
 # the input folders for the C++ sources
-INPUT="-I./jni"
+export NDK_PROJECT_PATH=./src/main/cpp
+INPUT="-I./src/main/cpp"
 
+# flush/create output folders
+rm -rf libs
+mkdir libs
 rm -rf src/main/java/nl/igorski/lib/audio/mwengine
 mkdir -p src/main/java/nl/igorski/lib/audio/mwengine
 
-swig -java -package nl.igorski.lib.audio.mwengine -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/main/java/nl/igorski/lib/audio/mwengine -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o jni/jni/java_interface_wrap.cpp mwengine.i
+# compile Java interface classes
 
-$ANDROID_NDK_ROOT/ndk-build V=1 $1
+swig -java -package nl.igorski.lib.audio.mwengine -DNO_RTIO -c++ -lcarrays.i -verbose -outdir src/main/java/nl/igorski/lib/audio/mwengine -I/usr/local/include -I/System/Library/Frameworks/JavaVM.framework/Headers $INPUT -o src/main/cpp/jni/java_interface_wrap.cpp src/main/cpp/mwengine.i
+
+# explicitly provide paths to .mk files as default ndk-build expects files in a folder called ./jni
+$ANDROID_NDK_ROOT/ndk-build -C $NDK_PROJECT_PATH APP_BUILD_SCRIPT=Android.mk NDK_APPLICATION_MK=Application.mk NDK_LIBS_OUT=../../../libs V=1 $1
