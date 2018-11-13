ca9c500373918dd528ac16c7df64006dce3b0f19
Igor Zinken <igor.zinken@igorski.nl>
2018-11-13 21:39:33 +0100

    Added missing null pointer assignment to Phaser destructor

--- src/main/cpp/processors/phaser.cpp
+++ src/main/cpp/processors/phaser.cpp
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2013-2017 Igor Zinken - http://www.igorski.nl
+ * Copyright (c) 2013-2018 Igor Zinken - http://www.igorski.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -60,6 +60,7 @@ Phaser::~Phaser()
         _alps->erase( _alps->begin() + i );
     }
     delete _alps;
+    _alps = nullptr;
 }
 
 /* public methods */
